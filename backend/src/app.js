16a5616c903b952a145d30c45ceea0ed68e1a373
salmanf91 <salmanfrs91@gmail.com>
2025-10-19 09:41:01 +0530

    Fix db connection code

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,14 +6,13 @@ const app = express();
 
 connectDB()
     .then(() => {
-        console.log("Database is connected")
+        console.log("Database is connected");
+        app.listen(PORT, ()=> {
+            console.log(`Server is listenig on ${PORT}`)
+        })
     })
-    .error((err) => {
+    .catch((err) => {
         console.log("Can not be connected to database", err)
     })
 
 const PORT = 7777;
-
-app.listen(PORT, ()=> {
-    console.log(`Server is listenig on ${PORT}`)
-})
