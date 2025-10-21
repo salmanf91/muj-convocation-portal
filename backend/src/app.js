e8d101529a9cdf143381555e29c6e11dd67f29a6
salmanf91 <salmanfrs91@gmail.com>
2025-10-21 09:07:24 +0530

    init database

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,27 @@ const express = require('express')
 const connectDB = require('./config/database')
 
 const app = express();
+const User = require('./models/user');
+
+app.post('/signup', async (req, res)=> {
+    const userObj = {
+        fistName: "Salman",
+        lastName: "Farees",
+        emailId: "salmanfrs91@gmail.com",
+        password: "Eternal@123"
+    }
+
+    //Creating a new instance of user model.
+    const user = new User(userObj);
+
+    try {
+        await user.save();
+        res.send("User Added Successfully");
+    } catch(err) {
+        res.status(400).send("Error saving the user:"  + err.message);
+    }
+
+});
 
 connectDB()
     .then(() => {
