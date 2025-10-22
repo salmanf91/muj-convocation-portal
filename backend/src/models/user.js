528836fa8115327e44314183ef42562616f09cfb
salmanf91 <salmanfrs91@gmail.com>
2025-10-22 15:40:59 +0530

    Data Sanitization and Schema

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema({
     firstName: {
@@ -15,11 +16,21 @@ const userSchema = new mongoose.Schema({
         required: true,
         unique: true,
         lowercase: true,
-        trim: true
+        trim: true,
+        validate(value) {
+            if(!validator.isEmail(value)) {
+                throw new Error("Invalid email id: " + value)
+            }
+        }
     },
     password: {
         type: String,
-        required: true
+        required: true,
+        validate(value) {
+            if(validator.isStrongPassword(value)) {
+                throw new Error("Enter a strong password: " +value)
+            }
+        }
     },
     gender: {
         type: "String",
@@ -32,6 +43,11 @@ const userSchema = new mongoose.Schema({
     photoUrl: {
         type: String,
         default: "https://t3.ftcdn.net/jpg/14/32/54/48/240_F_1432544801_KCFe2R9rk24i96JsyMCv88wOkhRH2c7U.jpg",
+        validate(value) {
+            if(!validator.isURL(value)) {
+                throw new Error("Invalid URL", +value)
+            } 
+        }
     },
     about: {
         type: String,
