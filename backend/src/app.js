69afdb1f0e18d7ef584c701b93eae813a8dca849
salmanf91 <salmanfrs91@gmail.com>
2025-11-03 15:40:17 +0530

    JWT Authentication

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,9 +7,11 @@ const {validateSignUpData} = require('./utils/validation')
 const app = express();
 const User = require('./models/user');
 const bcrypt = require('bcrypt');
-const { default: mongoose } = require('mongoose');
+const cookieParser = require('cookie-parser');
+const { userAuth } = require("./middlewares/auth")
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.get("/user", async(req, res) => {
     const email = req.body.emailId;
@@ -57,15 +59,15 @@ app.post('/signup', async (req, res)=> {
     try {
         validateSignUpData(req);
 
-        const {firstName, lastName, emailId, password} = req.body
+        const {firstName, lastName, emailId, password, gender} = req.body
         const passwordHash = await bcrypt.hash(password, 10);
-        console.log(passwordHash);
 
         const user = new User({
             firstName,
             lastName,
             emailId,
             password: passwordHash,
+            gender,
         });
         
         await user.save();
@@ -78,21 +80,48 @@ app.post('/signup', async (req, res)=> {
 
 app.post('/login', async (req, res) => {
     try {
-        const {emailId, password} = req.body
+        const { emailId, password } = req.body
 
         const user = await User.findOne({ emailId : emailId})
         if(!user) {
             throw new Error("Invalid Credentials")
         }
         
-        const isPasswordValid = await bcrypt.compare(password, user.password)
+        const isPasswordValid = await user.validatePassword(password)
+
         if(!isPasswordValid) {
             throw new Error("Invalid Credentials")
         } else {
+            const token = await user.getJWT();
+
+            res.cookie("token", token)
             res.send("User login successful")
         }
     } catch(err) {
         throw new Error("ERROR: " + err)
+    }
+})
+
+app.get('/profile', userAuth, async (req, res) => {
+    try {
+        // const cookies  = req.cookies
+        // const { token } = cookies;
+        
+        // if(!token) {
+        //     throw new Error ("Invalid Credentials");
+        // }
+        // const decodedMessage = await jwt.verify(token, "Muj@convocation-portal@123")
+        // const { _id } = decodedMessage
+
+        const user = req.user;
+        console.log(user)
+        // if(!user) {
+        //     throw new Error("Invalid Credentials")
+        // }
+
+        res.send(user);
+    } catch (err) {
+        throw new Error ("ERROR: " +err.message)
     }
 })
 
@@ -126,7 +155,6 @@ app.patch('/updateUser/:userId', async(req, res) => {
             throw new Error("Skills should not exceed 10")
         }
         const user = await User.findByIdAndUpdate(userId, data, {returnDocument: "before", runValidators: true})
-        console.log(user);
         res.send("data updated successfully");
     } catch (err) {
         res.status(400).send("Something went wrong: " + err.message);
---
+++ backend/src/middlewares/auth.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
+const userAuth = async (req, res, next) => {
+    try {
+        const {token} = req.cookies;
+        if(!token) {
+            throw new Error ("Token is not valid");
+        }
+
+        const decodedObj = await jwt.verify(token, "Muj@convocation-portal@123")
+
+        const { _id } = decodedObj;
+
+        const user = await User.findById(_id);
+        if(!user) {
+            throw new Error("User data not found")
+        }
+
+        req.user = user
+        next();
+    } catch (err) {
+        res.status(400).send('ERROR: '+err.message)
+    }
+} 
+
+module.exports = {
+    userAuth,
+}
